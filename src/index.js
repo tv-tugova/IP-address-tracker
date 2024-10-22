6bbc9c235245a1af414875a51270c9a4d9769a69
Tatiana <tvtugova1511@gmail.com>
2024-10-22 18:21:27 +0300

    Implement input validation and render IP data to page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
+import {validateIp} from './helpers';
+
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('button');
+
+const ipInfo = document.querySelector('#ip');
+const locationInfo = document.querySelector('#location');
+const timezoneInfo = document.querySelector('#timezone');
+const ispInfo = document.querySelector('#isp');
 
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
 
 function getData() {
-    fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_gTHqLPpOKk14eXyj8pLmTLWruofe3&ipAddress=${ipInput.value}`)
-        .then(response => response.json())
-        .then(console.log)
+    if (validateIp(ipInput.value)) {
+        fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_gTHqLPpOKk14eXyj8pLmTLWruofe3&ipAddress=${ipInput.value}`)
+            .then(response => response.json())
+            .then(data => setInfo(data))
+    }
 }
 
 function handleKey(e) {
     if (e.key === "Enter") {
         getData();
     }
+}
+
+function setInfo(mapData) {
+    console.log(mapData);
+    ipInfo.innerText = mapData.ip;
+    locationInfo.innerText = mapData.location.country + ' ' + mapData.location.region;
+    timezoneInfo.innerText = mapData.location.timezone;
+    ispInfo.innerText = mapData.isp;
 }
