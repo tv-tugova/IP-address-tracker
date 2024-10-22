a204557e3a9b6e22fc8885a8727cea541f69b768
Tatiana <tvtugova1511@gmail.com>
2024-10-22 14:36:51 +0300

    Fetch and log IP address data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('hello');
+const ipInput = document.querySelector('.search-bar__input');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', getData);
+ipInput.addEventListener('keydown', handleKey);
+
+function getData() {
+    fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_gTHqLPpOKk14eXyj8pLmTLWruofe3&ipAddress=${ipInput.value}`)
+        .then(response => response.json())
+        .then(console.log)
+}
+
+function handleKey(e) {
+    if (e.key === "Enter") {
+        getData();
+    }
+}
